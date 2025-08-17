8b3f89a5b46e6eaabaac70b2d92596a48fee973a
Rajdeep Tomar <rajdeeptomar527@gmail.com>
2025-08-17 18:14:02 +0530

    started new project

---
+++ netflix/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='font-bold text-4xl'> jai shree ram</div>
+    </>
+  )
+}
+
+export default App
